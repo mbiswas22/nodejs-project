7eacf569eb748d17f3ce31551fbdade2bbd472bb
mbiswas22 <moutusi646@yahoo.com>
2025-08-25 10:06:36 -0500

    Adding express and API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js app!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
